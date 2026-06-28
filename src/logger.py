0e7dd9098ae4efbba62234e65861a16c1181dac1
livvyyRUS <rem.game.on@gmail.com>
2026-06-28 17:53:26 +0300

    logger prettier

--- src/logger.py
+++ src/logger.py
@@ -9,3 +9,8 @@ logger.add(
     sys.stderr, 
     format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
 )
+
+logger.add(
+    "logs/app_{time:YYYY-MM-DD_HH-mm-ss}.log",
+    format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
+)
